943263261416e29a42f5fadf1bef2ed047345eae
Jules Joseph <julesj93@gmail.com>
2022-11-06 15:30:56 -0500

    edited prompts to match table/database info

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-const mysql = require("mysql2");
-
-
-
-
-
 
 
 
@@ -20,6 +14,63 @@ inquirer
   ])
   .then(answers => {
     console.info('Answer:', answers.directory);
+
+    inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the Employee name?',
+      validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log ("Please enter the manager's name!");
+            return false; 
+        }
+    }
+},
+{
+  type: 'input',
+  name: 'id',
+  message: "Please enter the manager's ID.",
+  validate: nameInput => {
+      if  (isNaN(nameInput)) {
+          console.log ("Please enter the employee's ID!")
+          return false; 
+      } else {
+          return true;
+      }
+  }
+},
+{
+  type: 'input',
+  name: 'email',
+  message: "Please enter the employees role.",
+  validate: email => {
+      valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+      if (valid) {
+          return true;
+      } else {
+          console.log ('Please enter an email!')
+          return false; 
+      }
+  }
+},
+{
+    type: 'input',
+    name: 'officeNumber',
+    message: "Please enter the employees salary.",
+    validate: nameInput => {
+        if  (isNaN(nameInput)) {
+            console.log ("Please enter managers office number!")
+            return false; 
+        } else {
+            return true;
+        }
+    }
+  },
+])
   });
 
 
