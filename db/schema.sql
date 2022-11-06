a1e7556bae287635369d4c48ef0978fe5216de87
Jules Joseph <julesj93@gmail.com>
2022-11-06 12:02:55 -0500

    added data base table for dep,role and employee

--- db/schema.sql
+++ db/schema.sql
@@ -6,16 +6,18 @@ USE EmployeeTracker_db;
 CREATE TABLE departments (
   id INT PRIMARY KEY,
   department_name VARCHAR(30),
-  last_name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
   id INT,
-  course_title VARCHAR(30) NOT NULL,
-  instructor_id INT,
+  role_title VARCHAR(30) NOT NULL,
+  role_salary DECIMAL,
   order_details TEXT,
-  FOREIGN KEY (instructor_id)
-  REFERENCES instructors(id)
-  ON DELETE SET NULL
+  department_id INT,
 );
+
+CREATE TABLE employees (
+
+    
+)
