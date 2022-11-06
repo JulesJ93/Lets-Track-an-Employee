6087a84d5ccda5a3df4cbe6bab85a4895e4380b1
Jules Joseph <julesj93@gmail.com>
2022-11-06 11:59:48 -0500

    added s to table names

--- db/schema.sql
+++ db/schema.sql
@@ -3,14 +3,14 @@ CREATE DATABASE EmployeeTracker_db;
 
 USE EmployeeTracker_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id INT PRIMARY KEY,
   department_name VARCHAR(30),
   last_name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
   id INT,
   course_title VARCHAR(30) NOT NULL,
   instructor_id INT,
