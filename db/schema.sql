2acea32e808498edf04d6bd7c399c69f8bf96878
Jules Joseph <julesj93@gmail.com>
2022-11-06 12:16:51 -0500

    added on delete set to null

--- db/schema.sql
+++ db/schema.sql
@@ -14,6 +14,7 @@ CREATE TABLE roles (
   role_salary DECIMAL,
   order_details TEXT,
   department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
@@ -21,7 +22,8 @@ CREATE TABLE employees (
     first_name VARCHAR (30),
     last_name VARCHAR (30),
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
 
