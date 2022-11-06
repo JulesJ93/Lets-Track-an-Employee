bc7fe538ae1272bb1f42d50b8f94a8d6775f5e89
Jules Joseph <julesj93@gmail.com>
2022-11-06 12:50:05 -0500

    finished seed for role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,6 +6,11 @@ VALUES ("Engineering"),
        ("Sales");
        
 INSERT INTO roles (role_title,role_salary,department_id)
-VALUES ("Sales Lead","100000","sales")
-       ("Salesperson","80000","sales" )
-       ("Lead Engineer")
+VALUES ("Sales Lead","100000","Sales")
+       ("Salesperson","80000","Sales" )
+       ("Lead Engineer","150000","Engineering")
+       ("Software Engineer","120000","Engineering")
+       ("Account Manager","160000","Finance")
+       ("Accountant","125000","Finance")
+       ("Legal Team Lead","250000","Legal")
+       ("Lawyer","190000","Legal")
