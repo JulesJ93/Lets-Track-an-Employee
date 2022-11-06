9ccb31f432880b95df6b08a7c15e923fdb6bc416
Jules Joseph <julesj93@gmail.com>
2022-11-06 12:11:41 -0500

    added employee data base now adding seeds

--- db/schema.sql
+++ db/schema.sql
@@ -18,6 +18,11 @@ CREATE TABLE roles (
 
 CREATE TABLE employees (
     id  INT PRIMARY KEY,
+    first_name VARCHAR (30),
+    last_name VARCHAR (30),
+    role_id INT,
+    manager_id INT
+);
 
 
 )
