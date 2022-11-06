49ae03780d7a50a49cb84f56a7b588e09c921dbb
Jules Joseph <julesj93@gmail.com>
2022-11-06 13:56:35 -0500

    tet

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ inquirer
   .prompt([
     {
       type: 'list',
-      name: 'reptile',
+      name: 'directory',
       message: 'What would you like to view?',
       choices: ['View All Employees', 'Add Employee','Update Employee Role','View All Roles','Add Role','View All Departments','Add Department'],
     },
   ])
   .then(answers => {
-    console.info('Answer:', answers.reptile);
+    console.info('Answer:', answers.directory);
   });
